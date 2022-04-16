98b64110ade45489e1b377c285f485c575468712
RHONT <rg915i@yandex.ru>
2022-04-16 22:40:19 +0300

    Подогнал под стандартны спринга все классы
    Думал, что спринг изменит проект координально. Но видимо почва не та... Больше возни... Спринг пока не раскурил...

---
+++ src/main/java/Test_ternary/Child.java
@@ -0,0 +1,12 @@
+package Test_ternary;
+
+import java.util.Arrays;
+
+public class Child extends Parent {
+    int[] abc = {2, 2, 1, 1};
+
+    public Child() {
+        System.out.println(Arrays.toString(abc));
+
+    }
+}
